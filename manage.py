ad1586869669036287125641ce4e059d69e54633
Teyras <Teyras@gmail.com>
2017-09-11 09:56:11 +0200

    replace flask-script with click

--- manage.py
+++ manage.py
@@ -1,19 +1,29 @@
-from flask_script import Manager, Shell
+import click
+from flask.cli import shell_command
 
 from cxworker.worker import Worker
 
 worker = Worker()
-manager = Manager(worker.app, with_default_commands=False)
-manager.add_command("shell", Shell())
 
 
-@manager.option("-h", "--host", dest="host", default="", help="The host name to which the HTTP API should bind")
-@manager.option("-p", "--port", dest="port", default=5000, help="The port to which the HTTP API should bind")
-@manager.option("-c", "--config", dest="config_file", default="cxworker.yml", help="Path to a configuration file")
+@click.group()
+@click.pass_context
+def cli(ctx):
+    ctx.obj = worker.app
+
+
+@click.option("-h", "--host", dest="host", default="", help="The host name to which the HTTP API should bind")
+@click.option("-p", "--port", dest="port", default=5000, help="The port to which the HTTP API should bind")
+@click.option("-c", "--config", dest="config_file", default="cxworker.yml", help="Path to a configuration file")
 def run_worker(host, port, config_file):
     with open(config_file, "r") as config:
         worker.load_config(config)
 
     worker.run(host, port)
 
-manager.run()
+
+cli.add_command(shell_command)
+cli.add_command(run_worker)
+
+if __name__ == "__main__":
+    cli()
