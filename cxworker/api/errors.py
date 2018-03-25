d245f7430419c73c5afc3d81e2d595ae0c1d3cdf
Adam Blazek <blazekada@gmail.com>
2018-03-25 01:09:37 +0100

    Add custom exception descriptions

--- cxworker/api/errors.py
+++ cxworker/api/errors.py
@@ -1,14 +1,14 @@
 class AppError(Exception, BaseException):
-    pass
+    """Base application exception, will result in 500 response."""
 
 
 class ClientActionError(AppError):
-    pass
+    """Base client-side error exception, will result in 400 response."""
 
 
 class UnknownContainerError(ClientActionError):
-    pass
+    """Exception raised when application attempts to use container with unknown id."""
 
 
-class StorageError(Exception):
-    pass
+class StorageError(AppError):
+    """Exception raised when application encounters some issue with the minio storage."""
