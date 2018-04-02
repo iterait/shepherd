1936157ae60ac31121d0a0a5c2a82ceba79f4455
Adam Blazek <blazekada@gmail.com>
2018-04-02 20:38:50 +0200

    Add utils tests

---
+++ tests/conftest.py
@@ -0,0 +1,39 @@
+import pytest
+from minio import Minio
+import subprocess
+import os
+import random
+import sys
+import string
+import os.path as path
+
+from cxworker.utils import create_clean_dir
+
+
+@pytest.fixture(scope='session')
+def minio():
+    data_dir = path.join(create_clean_dir(path.join('/tmp', 'minio')))
+    env = os.environ.copy()
+    minio_key = 'AKIAIOSFODNN7EXAMPLE'
+    minio_secret = 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY'
+    env['MINIO_ACCESS_KEY'] = minio_key
+    env['MINIO_SECRET_KEY'] = minio_secret
+    minio_host = '0.0.0.0:7000'
+    proc = subprocess.Popen(['minio', 'server', '--address', minio_host, data_dir], env=env)
+    yield Minio(minio_host, access_key=minio_key, secret_key=minio_secret, secure=False)
+    proc.kill()
+
+
+@pytest.fixture()
+def bucket(minio: Minio):
+    request_id = 'test-request-'+(''.join(random.choices(string.ascii_lowercase + string.digits, k=10)))
+    if minio.bucket_exists(request_id):
+        for obj in minio.list_objects_v2(request_id, recursive=True):
+            minio.remove_object(request_id, obj.object_name)
+        minio.remove_bucket(request_id)
+    minio.make_bucket(request_id)
+    yield request_id
+    if minio.bucket_exists(request_id):
+        for obj in minio.list_objects_v2(request_id, recursive=True):
+            minio.remove_object(request_id, obj.object_name)
+        minio.remove_bucket(request_id)
