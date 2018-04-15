0a301eab179ecbe76aabc1a2dc82dcd13963c812
Adam Blazek <blazekada@gmail.com>
2018-04-15 20:34:15 +0200

    Add Shepherd tests, Add gevent .coveragerc

--- tests/shepherd/conftest.py
+++ tests/shepherd/conftest.py
@@ -1,8 +1,12 @@
 import pytest
+import json
+import os.path as path
+from io import BytesIO
 import os.path as path
 
 from cxworker.shepherd.config import load_worker_config
 from cxworker.shepherd import Shepherd
+from cxworker.api.models import ModelModel
 
 
 @pytest.fixture()
@@ -25,5 +29,39 @@ def valid_config(valid_config_file):
 def shepherd(valid_config, minio):
     shepherd = Shepherd(valid_config.sheep, valid_config.data_root, minio, valid_config.registry)
     yield shepherd
-    shepherd.slaughter_all()
-    shepherd.notifier.close()
+    shepherd.close()
+
+
+@pytest.fixture()
+def job(bucket, minio):
+    job_id = bucket
+    data = json.dumps({'key': [1000]}).encode()
+    minio.put_object(bucket, 'inputs/input.json', BytesIO(data), len(data))
+    yield job_id, ModelModel(dict(name='cxflow-test', version='latest'))
+
+
+@pytest.fixture()
+def bad_job(bucket, minio):
+    """Job with wrong input name should cause recoverable runner runtime error."""
+    job_id = bucket
+    data = json.dumps({'key': [1000]}).encode()
+    minio.put_object(bucket, 'inputs/some_other_file.json', BytesIO(data), len(data))
+    yield job_id, ModelModel(dict(name='cxflow-test', version='latest'))
+
+
+@pytest.fixture()
+def bad_configuration_job(bucket, minio):
+    """Job with wrong model name should cause sheep configuration error."""
+    job_id = bucket
+    data = json.dumps({'key': [1000]}).encode()
+    minio.put_object(bucket, 'inputs/input.json', BytesIO(data), len(data))
+    yield job_id, ModelModel(dict(name='wrong-model', version='latest'))
+
+
+@pytest.fixture()
+def bad_runner_job(bucket, minio):
+    """cxflow-test:test model has bad runner configuration; thus, the runner should fail (and stop)."""
+    job_id = bucket
+    data = json.dumps({'key': [1000]}).encode()
+    minio.put_object(bucket, 'inputs/input.json', BytesIO(data), len(data))
+    yield job_id, ModelModel(dict(name='cxflow-test', version='test'))
--- tests/shepherd/test_shepherd.py
+++ tests/shepherd/test_shepherd.py
@@ -1,10 +1,14 @@
+import json
+
+import gevent
 import pytest
 
 
 from cxworker.sheep import BareSheep, DockerSheep, SheepConfigurationError
 from cxworker.shepherd import Shepherd
-from cxworker.api.errors import UnknownSheepError
+from cxworker.api.errors import UnknownSheepError, UnknownJobError
 from cxworker.shepherd.config import WorkerConfig
+from cxworker.utils.storage import minio_object_exists
 
 
 def test_shepherd_init(valid_config: WorkerConfig, minio):
@@ -35,3 +39,48 @@ def test_shepherd_status(shepherd):
     sheep_name, sheep = next(shepherd.get_status())
     assert not sheep.running
     assert sheep_name == 'bare_sheep'
+
+
+def test_job(shepherd, job, minio):
+    job_id, job_meta = job
+
+    with pytest.raises(UnknownJobError):
+        shepherd.is_job_done(job_id)
+
+    shepherd.enqueue_job(job_id, job_meta)
+    assert not shepherd.is_job_done(job_id)
+    gevent.sleep(1)
+    assert shepherd['bare_sheep'].running
+    assert shepherd.is_job_done(job_id)
+    assert minio_object_exists(minio, job_id, 'outputs/output.json')
+    assert minio_object_exists(minio, job_id, 'done')
+    output = json.loads(minio.get_object(job_id, 'outputs/output.json').read().decode())
+    assert output['key'] == [1000]
+    assert output['output'] == [1000*2]
+
+
+def test_failed_job(shepherd, bad_job, minio):
+    job_id, job_meta = bad_job
+    shepherd.enqueue_job(job_id, job_meta)  # runner should fail to process the job (and send an ErrorMessage)
+    gevent.sleep(1)
+    assert shepherd['bare_sheep'].running
+    assert shepherd.is_job_done(job_id)
+    assert minio_object_exists(minio, job_id, 'error')
+
+
+def test_bad_configuration_job(shepherd, bad_configuration_job, minio):
+    job_id, job_meta = bad_configuration_job
+    shepherd.enqueue_job(job_id, job_meta)  # shepherd should get SheepConfigurationError
+    gevent.sleep(1)
+    assert not shepherd['bare_sheep'].running
+    assert shepherd.is_job_done(job_id)
+    assert minio_object_exists(minio, job_id, 'error')
+
+
+def test_bad_runner_job(shepherd, bad_runner_job, minio):
+    job_id, job_meta = bad_runner_job
+    shepherd.enqueue_job(job_id, job_meta)  # runner should not start (and health-check should discover it)
+    gevent.sleep(2)
+    assert not shepherd['bare_sheep'].running
+    assert shepherd.is_job_done(job_id)
+    assert minio_object_exists(minio, job_id, 'error')
