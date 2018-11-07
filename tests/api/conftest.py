bf6350eb5e3a5ba548053dfec0d3e58cc8d03be7
gdynusa <jana.horecka@cognexa.com>
2018-11-07 11:38:27 +0100

    Add api tests

--- tests/api/conftest.py
+++ tests/api/conftest.py
@@ -1,15 +1,27 @@
 from unittest import mock
-
 import pytest
 
 from shepherd.api import create_app
+from shepherd.api.models import SheepModel
 from shepherd.api.views import create_shepherd_blueprint
 from shepherd.shepherd import Shepherd
 
 
 @pytest.fixture(scope="function")
 def mock_shepherd():
-    yield mock.create_autospec(Shepherd)
+    def status_gen():
+        yield "bare_sheep", SheepModel({
+                "running": False,
+                "model": {
+                    "name": "model_1",
+                    "version": "latest"
+                }
+            })
+    m = mock.create_autospec(Shepherd)
+    m.get_status.side_effect = status_gen
+    m.is_job_done.return_value = True
+    m.notifier = mock.MagicMock()
+    yield m
 
 
 @pytest.fixture(scope="function")
